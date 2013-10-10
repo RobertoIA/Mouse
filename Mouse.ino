40c8e8c7d709d948ca731c282b5d2960be4ab1c0
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-10-10 17:43:00 +0200

    Functions for the two different movement types.
    Added movement test function.

--- Mouse.ino
+++ Mouse.ino
@@ -3,6 +3,8 @@
 #define SERVO_PIN 0
 #define CENTER 90
 #define LAT_ANGLE 10
+#define FAST_DELAY 20
+#define SLOW_DELAY 50
 
 Servo servo;
 int angle = 90; // current servo angle.
@@ -10,9 +12,32 @@ int dir = 0; // current servo direction.
 
 void setup() {
   servo.attach(SERVO_PIN);
+  servo.write(angle);
 }
 
 void loop() {
+  //test_movement();
+}
+
+// DEBUG
+void test_movement() {
+  for(int i = 0; i < 100; i++)
+    move_fast();
+  for(int i = 0; i < 100; i++)
+    move_slow();
+}
+
+void move_fast() {
+  move_servo();
+  delay(FAST_DELAY);
+}
+
+void move_slow() {
+  move_servo();
+  delay(SLOW_DELAY);
+}
+
+void move_servo() {
   if(dir == 0) {
     angle += 1;
     if(angle >= CENTER + LAT_ANGLE) dir = 1;
@@ -22,8 +47,10 @@ void loop() {
     if(angle <= CENTER - LAT_ANGLE) dir = 0;
   }
   servo.write(angle);
-  delay(50);
 }
+
+
+
 
 
 
