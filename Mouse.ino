a3ccf268aed7c4bd14fa270a929566ca3b7a0ef9
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-10-12 00:00:17 +0200

    Added presence detection.

--- Mouse.ino
+++ Mouse.ino
@@ -1,22 +1,34 @@
 #include <Servo.h>
 
 #define SERVO_PIN 0
+#define PIR_PIN 0 // Analog pin 0
+#define LED_PIN 13
+
 #define CENTER 90
 #define LAT_ANGLE 10
 #define FAST_DELAY 20
 #define SLOW_DELAY 50
 
 Servo servo;
+int PIR_val = 0;
 int angle = 90; // current servo angle.
 int dir = 0; // current servo direction.
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
   servo.write(angle);
   servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  test_movement();
+  if(detects_movement()) {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  //test_movement();
 }
 
 // DEBUG
@@ -25,6 +37,10 @@ void test_movement() {
     move_fast();
   for(int i = 0; i < 100; i++)
     move_slow();
+}
+
+boolean detects_movement() {
+  return analogRead(PIR_PIN) >= 200;
 }
 
 void move_fast() {
@@ -48,6 +64,7 @@ void move_servo() {
   }
   servo.write(angle);
 }
+
 
 
 
