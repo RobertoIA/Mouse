3dc5278252e59bad3cade3ce883960c4530fe827
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-10-15 20:34:38 +0200

    Documented.

--- Mouse.ino
+++ Mouse.ino
@@ -1,18 +1,18 @@
 #include <Servo.h>
 
-#define SERVO_PIN 10
-#define PIR_PIN 11
-#define LED_PIN 13
+#define SERVO_PIN 10 // digital pin 10
+#define PIR_PIN 11 // digital pin 11
+#define LED_PIN 13 // digital pin 13
 
-#define CENTER 90
-#define LAT_ANGLE 10
-#define FAST_DELAY 20
-#define SLOW_DELAY 100
+#define CENTER 90 // rotation center
+#define LAT_ANGLE 10 // max angle of rotation (both sides)
+#define FAST_DELAY 20 // delay on fast movement
+#define SLOW_DELAY 100 // delay on slow movement
 
 Servo servo;
-int PIR_val = 0;
-int angle = 90; // current servo angle.
-int dir = 0; // current servo direction.
+int PIR_val = 0; // val of the PIR sensor
+int angle = 90; // current servo angle
+int dir = 0; // current servo direction
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
@@ -20,7 +20,9 @@ void setup() {
   servo.write(angle);
   servo.attach(SERVO_PIN);
 
+  // delay to allow the PIR to calibrate
   delay(15000);
+  // ready indicator
   for(int i = 0; i < 10; i++) {
     digitalWrite(LED_PIN, HIGH);
     delay(100);
@@ -50,10 +52,12 @@ void test_movement() {
     move_servo(SLOW_DELAY);
 }
 
+// checks the PIR sensor
 boolean detects_movement() {
   return digitalRead(PIR_PIN) == HIGH;
 }
 
+// moves the servo 1 degree
 void move_servo(int servo_delay) {
   if(dir == 0) {
     angle += 1;
